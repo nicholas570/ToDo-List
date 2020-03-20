791d069d4702429695c9efc421a9cd5ab75db2fd
nicholas570 <nicolas.mercier570@gmail.com>
2020-03-20 18:24:39 +0100

    random greeting function & time

---
+++ script.js
@@ -0,0 +1,25 @@
+// Random greetings functions
+
+const greetingsArray = ["what's up today?", "Did you sleep well?", "Damn! it's early to work",
+    "I missed you", "Have a lovely day"
+];
+
+const getRandomGreetings = (array) => {
+    let randomIndex = Math.floor((Math.random() * array.length));
+    return randomIndex;
+};
+
+document.getElementById('greetings').innerHTML = greetingsArray[getRandomGreetings(greetingsArray)];
+
+// Date
+
+const hour = new Date().getHours();
+const minute = new Date().getMinutes();
+const time = `${hour}:${minute}`;
+document.getElementById('time').innerHTML = time;
+
+
+// Add a task to the list
+
+const taskToAdd = document.getElementById('taskToAdd').value;
+console.log(taskToAdd);
